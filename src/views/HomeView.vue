dbd86dfd269846efc31259da5f38600d0f80540f
O-cdy <1940256508@qq.com>
2022-10-14 17:19:04 +0800

    update HomeView.vue and vue.config.js(set up a proxy to route the request to the endpoints)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <PaginatedBookings msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+// import PaginatedBookings from '@/components/PaginatedBookings.vue'
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
+    // PaginatedBookings
   }
 }
 </script>
